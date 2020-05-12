68e7778ea69dedfeeba370e14436e926a547bd8c
Paul Mackey <mackey.paul.23@gmail.com>
2020-05-12 11:20:23 +0100

    Adds legacy project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import Post from '@/views/Post.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/foo', component: Post }
+]
+
+const router = new VueRouter({
+  routes
+  // mode: 'history', // will need htaccess for this to work
+  // base: process.env.NODE_ENV === 'development' ? '/' : '/toyota/',
+})
+*/
 Vue.config.productionTip = false
 
 new Vue({
