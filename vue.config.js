2e69d7309fad190515e5a2a0cb095911fe88059b
Paul Mackey <mackey.paul.23@gmail.com>
2020-08-07 21:59:58 +0100

    Fixes build error on Netlify

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 // vue.config.js
 module.exports = {
-  runtimeCompiler: true,
-  less:{
-    // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
-    // `primary` is global variables fields name
-    globalVars: {
-      primary: '#fff'
-    }
-  }
+  runtimeCompiler: true
 }
