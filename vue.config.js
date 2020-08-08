c2bbdd7c870779257d49d95d71e34c3f943f80e3
Paul Mackey <mackey.paul.23@gmail.com>
2020-08-08 22:40:21 +0100

    prerender blog page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 // vue.config.js
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: ['/blog/'],
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      // We need to inject a value so we're able to
+      // detect if the page is currently pre-rendered.
+      inject: {},
+      // Our view component is rendered after the API
+      // request has fetched all the necessary data,
+      // so we create a snapshot of the page after the
+      // `data-view` attribute exists in the DOM.
+      renderAfterTime: 5000
+    }),
+  }),
+];
+
 module.exports = {
-  runtimeCompiler: true
-}
+  lintOnSave: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
+};
